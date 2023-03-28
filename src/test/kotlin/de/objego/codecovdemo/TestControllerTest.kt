8f50e7e1221ce133f6830a5d44a5baf6efcb9d47
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2023-03-28 11:35:29 +0200

    Cover TestController

---
+++ src/test/kotlin/de/objego/codecovdemo/TestControllerTest.kt
@@ -0,0 +1,11 @@
+package de.objego.codecovdemo
+
+import org.junit.jupiter.api.Test
+
+class TestControllerTest {
+
+    @Test
+    fun getTestData() {
+        TestController().getTestData()
+    }
+}
